39854b62af09e2787732a90d81f7bfdee8e0a80b
Aman <aaaman.sh007@gmail.com>
2025-02-07 14:26:12 +0530

    initial commit

---
+++ src/GoogleTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class GoogleTest {
+    @Test
+    public void HomePageCheck() throws URISyntaxException, MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+//        caps.setPlatform(Platform.WINDOWS);
+//        caps.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+//        caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+
+        WebDriver driver = new RemoteWebDriver(new URI("http://192.168.1.139:4444").toURL(), caps );
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+
+    }
+}
